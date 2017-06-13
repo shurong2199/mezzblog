420df610507acc0577b9123159bcdb73fb857d05
Wang Shurong <wangshurong@kavout.com>
2017-06-13 19:23:16 +0800

    feat: 修改项目数据库为 MySQL

--- mezzblog/local_settings.py
+++ mezzblog/local_settings.py
@@ -7,21 +7,32 @@ DEBUG = True
 SECRET_KEY = "6^0l9z!(y3hn2t4qhe)oo=_569b*-oxs9fy10pq0^eqoi7sbhg"
 NEVERCACHE_KEY = "p5a@pas&2=u3oj1!a_(p&m7okk=cl4km)g-!3!w#%hg+$sua=v"
 
+# DATABASES = {
+#     "default": {
+#         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+#         "ENGINE": "django.db.backends.sqlite3",
+#         # DB name or path to database file if using sqlite3.
+#         "NAME": "dev.db",
+#         # Not used with sqlite3.
+#         "USER": "",
+#         # Not used with sqlite3.
+#         "PASSWORD": "",
+#         # Set to empty string for localhost. Not used with sqlite3.
+#         "HOST": "",
+#         # Set to empty string for default. Not used with sqlite3.
+#         "PORT": "",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.sqlite3",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "dev.db",
-        # Not used with sqlite3.
-        "USER": "",
-        # Not used with sqlite3.
-        "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mezzblog',
+        'USER': 'root',
+        'PASSWORD': 'kavout',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
 }
 
 ###################
@@ -29,7 +40,7 @@ DATABASES = {
 ###################
 
 # Domains for public site
-# ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["*"]
 
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
@@ -46,3 +57,6 @@ DATABASES = {
 #     "SECRET_KEY": SECRET_KEY,
 #     "NEVERCACHE_KEY": NEVERCACHE_KEY,
 # }
+
+import pymysql
+pymysql.install_as_MySQLdb()
