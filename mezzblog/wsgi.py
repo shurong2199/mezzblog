f0a70478577bd5c5d7b56c2dfeb15cbfd3232f47
Wang Shurong <wangshurong@kavout.com>
2017-06-13 14:03:52 +0800

    feat: 分离出独立的 mezzblog 项目

--- mezzblog/wsgi.py
+++ mezzblog/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for myproject project.
+WSGI config for mezzblog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("myproject"))
+                      "%s.settings" % real_project_name("mezzblog"))
 
 application = get_wsgi_application()
